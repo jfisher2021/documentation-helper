87ff3364328186c10c1a3ee91ee9c01f44c7667f
Jonathan Fisher <jfisherd@emeal.nttdata.com>
2025-07-29 10:27:07 +0200

    Update Pipfile to require Python 3.13 to match the installed version

---
+++ ingestion.py
@@ -0,0 +1,51 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+import certifi
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_header, log_info, log_success,
+                    log_warning)
+
+load_dotenv()
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-small", show_progress_bar=False, chunk_size=50, retry_min_seconds=10
+)
+
+vectorstore = PineconeVectorStore(
+    embedding=embedding,
+    index_name=os.getenv("INDEX_NAME"),
+)
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
+async def main():
+    """Main async function to orchestrate the workflow."""
+    log_header("VECTOR STORAGE PHASE")
+
+    log_info("TaviliCrawl: starting to crawl documentation from https://python.langchain.com/")
+
+    res = tavily_crawl.invoke({
+        "url": "https://python.langchain.com/",
+        "max_depth": 5,
+        "extract_depth": "advanced",
+    })
+    print(res)
+    log_success("TaviliCrawl: successfully crawled documentation")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ prueba.py
@@ -0,0 +1,4 @@
+import certifi
+print(certifi.where())
+import ssl
+print(ssl.get_default_verify_paths())
